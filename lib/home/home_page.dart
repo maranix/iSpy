e1a7be11112cc21672a964865fb1ab8ab6a15e8c
ramanverma2k <ramanverma4183@gmail.com>
2021-09-16 22:08:41 +0530

    home_page: initial layout
    * greet the player with their username.
    * Pick an image from the gallery using image_picker plugin.
    * Let the player take an image from their camera using image_picker plugin.

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,10 +1,52 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key, required this.username}) : super(key: key);
 
+  final String username;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final ImagePicker _picker = ImagePicker();
+    XFile? image, photo;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Welcome, $username'),
+              TextButton(
+                onPressed: () async => {
+                  image = await _picker.pickImage(source: ImageSource.gallery)
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                ),
+                child: const Text(
+                  'Pick an Image',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              TextButton(
+                onPressed: () async => {
+                  photo = await _picker.pickImage(source: ImageSource.camera)
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.blueAccent),
+                ),
+                child: const Text(
+                  'Capture an Image',
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
